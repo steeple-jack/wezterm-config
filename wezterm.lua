5a88cb83d54363b3419b498227429bb8f8e7c077
steeple-jack <58491590+steeple-jack@users.noreply.github.com>
2022-12-24 20:33:05 -0500

    cleaned up crap, added tab controls

--- wezterm.lua
+++ wezterm.lua
@@ -11,19 +11,15 @@ return {
   -- Key Assignments
   
   keys = {
-    {key="d", mods="ALT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-	{key="d", mods="ALT|SHIFT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-	{key="LeftArrow", mods="ALT", action=wezterm.action{ActivatePaneDirection="Left"}},
-	{key="RightArrow", mods="ALT", action=wezterm.action{ActivatePaneDirection="Right"}},
-	{key="UpArrow", mods="ALT", action=wezterm.action{ActivatePaneDirection="Up"}},
-	{key="DownArrow", mods="ALT", action=wezterm.action{ActivatePaneDirection="Down"}},
-	{key="h", mods="ALT", action=wezterm.action{ActivatePaneDirection="Left"}},
-	{key="l", mods="ALT", action=wezterm.action{ActivatePaneDirection="Right"}},
-	{key="k", mods="ALT", action=wezterm.action{ActivatePaneDirection="Up"}},
-	{key="j", mods="ALT", action=wezterm.action{ActivatePaneDirection="Down"}},
-	{key="z", mods="ALT", action="TogglePaneZoomState"},
-	{key="w", mods="ALT", action=wezterm.action.CloseCurrentPane{confirm = false}},
-	{key='q', mods="ALT", action = wezterm.action.QuitApplication },
+
+    -- Tab controls
+    {key='LeftArrow', mods="ALT", action=wezterm.action.ActivateTabRelative(-1)},
+    {key='RightArrow', mods="ALT", action=wezterm.action.ActivateTabRelative(1)},
+    {key='t', mods="ALT", action=wezterm.action.SpawnTab 'CurrentPaneDomain'},
+    
+    -- Close controls
+	  {key="w", mods="ALT", action=wezterm.action.CloseCurrentTab{confirm = false}},
+	  {key='q', mods="ALT", action = wezterm.action.QuitApplication },
   },
   
 }
